3eefa313e564ba7af10503e962081afc3ac0fea9
Brian Nicholls <nichol88@gmail.com>
2020-01-21 17:53:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -24,6 +24,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, DIFFERENCE(pledges.amount, projects.funding_goal)
   FROM projects
+  LEFT JOIN pledges ON projects.id = pledges.project_id
   WHERE pledges.amount > projects.funding_goal
   ;"
 end
