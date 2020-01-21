21b11bd74a3428f9b027806e37e1381e33d5c18d
Brian Nicholls <nichol88@gmail.com>
2020-01-21 19:14:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,6 +32,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount)
+  FROM users
   GROUP BY users.name
   ORDER BY SUM(pledges.amount)
   ;"
