4e2849fef5585f41a1c60d5c8e401dfc388b70a3
Brian Nicholls <nichol88@gmail.com>
2020-01-21 18:45:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,14 +15,14 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT users.name, users.age, pledges.amount FROM pledges
-  LEFT JOIN users ON pledges.user_id = users.id
+  "SELECT name, age, SUM(pledges.amount) FROM users
+  LEFT JOIN pledges ON users.id = pledges.user_id
   GROUP BY users.name
   ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, SUM(pledges.amount)
+  "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
   FROM projects
   LEFT JOIN pledges ON projects.id = pledges.project_id
   WHERE pledges.amount >= projects.funding_goal
